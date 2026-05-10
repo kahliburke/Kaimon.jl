19338cfb223683f1cc2a97019cda88e9b2824078
Carroll Vance <cs.vance@icloud.com>
2026-05-10 14:07:50 +0000

    Make XDG_CACHE_HOME handling consistent by appending kaimon

--- src/Kaimon.jl
+++ src/Kaimon.jl
@@ -40,15 +40,15 @@ Respects `XDG_CACHE_HOME` on Unix; uses `LOCALAPPDATA` on Windows.
 Defaults to `~/.cache/kaimon`.
 """
 function kaimon_cache_dir()
-    dir = get(ENV, "XDG_CACHE_HOME") do
-        if Sys.iswindows()
-            joinpath(
-                get(ENV, "LOCALAPPDATA", joinpath(homedir(), "AppData", "Local")),
-                "Kaimon",
-            )
-        else
-            joinpath(homedir(), ".cache", "kaimon")
-        end
+    dir = if haskey(ENV, "XDG_CACHE_HOME")
+        joinpath(ENV["XDG_CACHE_HOME"], "kaimon")
+    elseif Sys.iswindows()
+        joinpath(
+            get(ENV, "LOCALAPPDATA", joinpath(homedir(), "AppData", "Local")),
+            "Kaimon",
+        )
+    else
+        joinpath(homedir(), ".cache", "kaimon")
     end
     mkpath(dir)
     return dir
